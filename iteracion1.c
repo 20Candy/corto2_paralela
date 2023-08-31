459d31965c563befd6f6c97beaca4c1375b3e3ec
Carol Arevalo <60670458+20Candy@users.noreply.github.com>
2023-08-30 19:22:30 -0600

    wip

---
+++ iteracion1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Función recursiva para calcular el n-ésimo término de Fibonacci
+unsigned long long fib_recursive(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fib_recursive(n - 1) + fib_recursive(n - 2);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <n>\n", argv[0]);
+        return 1;
+    }
+
+    int n = atoi(argv[1]);
+
+    // Medir el tiempo de ejecución
+    clock_t start_time = clock();
+    unsigned long long result = fib_recursive(n);
+    clock_t end_time = clock();
+
+    double execution_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    printf("El %d-ésimo término de Fibonacci es: %llu\n", n, result);
+    printf("Tiempo de ejecución: %.6f segundos\n", execution_time);
+
+    return 0;
+}
